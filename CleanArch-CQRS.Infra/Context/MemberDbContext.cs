52a283ef67bfbf90165df497645f5d3867dbfa98
Rafael Brito <henrique8264@gmail.com>
2025-02-02 14:05:57 -0300

    feat(infra): add Member entity configuration and DbContext

---
+++ CleanArch-CQRS.Infra/Context/MemberDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArch_CQRS.Domain.Entities;
+using CleanArch_CQRS.Infra.EntityConfig;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch_CQRS.Infra.Context;
+public class MemberDbContext : DbContext
+{
+    public MemberDbContext(DbContextOptions<MemberDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Member> Members { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new MemberConfiguration());
+    }
+}
---
+++ CleanArch-CQRS.Infra/EntityConfig/MemberConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch_CQRS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArch_CQRS.Infra.EntityConfig;
+public class MemberConfiguration : IEntityTypeConfiguration<Member>
+{
+    public void Configure(EntityTypeBuilder<Member> builder)
+    {
+        builder.HasKey(m => m.Id);
+        builder.Property(m => m.FirstName).HasMaxLength(100).IsRequired();
+        builder.Property(m => m.LastName).HasMaxLength(100).IsRequired();
+        builder.Property(m => m.Gender).HasMaxLength(10).IsRequired();
+        builder.Property(m => m.Email).HasMaxLength(150).IsRequired();
+        builder.Property(m => m.IsActive).IsRequired();
+
+        builder.HasData(
+            new Member(1, "Jucá", "Pinhamangaba", "masculino", "johndoe@email.com", true),
+            new Member(2, "Maria", "Bonita", "feminino", "mariabonita@email.com", true)
+        );
+    }
+}
