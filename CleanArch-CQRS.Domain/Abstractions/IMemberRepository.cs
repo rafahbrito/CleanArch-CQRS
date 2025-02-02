6615685de407c631cf8aa9dfcdb163ef3b1de28d
Rafael Brito <henrique8264@gmail.com>
2025-02-02 13:22:27 -0300

    feat(Domain): implements interface IUnitOfWork

--- CleanArch-CQRS.Domain/Abstractions/IMemberRepository.cs
+++ CleanArch-CQRS.Domain/Abstractions/IMemberRepository.cs
@@ -3,9 +3,9 @@
 namespace CleanArch_CQRS.Domain.Abstractions;
 public interface IMemberRepository
 {
-    Task<IEnumerable<Member>> GetAll();
+    Task<IEnumerable<Member>> GetAllMembers();
     Task<Member> GetMemberById(int memberId);
     Task<Member> AddMember(Member member);
-    Task<Member> UpdateMember(Member member);
+    void UpdateMember(Member member);
     Task<Member> DeleteMember(int memberId);
 }
---
+++ CleanArch-CQRS.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace CleanArch_CQRS.Domain.Abstractions;
+public interface IUnitOfWork
+{
+    IMemberRepository MemberRepository { get; }
+    Task CommitAsync();
+}
