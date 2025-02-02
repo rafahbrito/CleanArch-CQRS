724ff826dece6203b978b92c3db987c037f94ead
Rafael Brito <henrique8264@gmail.com>
2025-02-02 17:14:24 -0300

    feat(infra): add dependency injection for database and repositories

---
+++ CleanArch-CQRS.CrossCutting/AppDependencies/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch_CQRS.Domain.Abstractions;
+using CleanArch_CQRS.Infra.Context;
+using CleanArch_CQRS.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch_CQRS.CrossCutting.AppDependencies;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var mySqlConnection = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<MemberDbContext>(options => options.UseSqlServer(mySqlConnection));
+
+        services.AddScoped<IMemberRepository, MemberRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
