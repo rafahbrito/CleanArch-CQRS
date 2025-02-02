0586b23fbcdae5d30173d0293fa7a4ada4131956
Rafael Brito <henrique8264@gmail.com>
2025-02-02 16:40:05 -0300

    feat(infra): add repository and UnitOfWork for Member entity

---
+++ CleanArch-CQRS.Infra/Repositories/MemberRepository.cs
@@ -0,0 +1,47 @@
+﻿using CleanArch_CQRS.Domain.Abstractions;
+using CleanArch_CQRS.Domain.Entities;
+using CleanArch_CQRS.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch_CQRS.Infra.Repositories;
+public class MemberRepository : IMemberRepository
+{
+    protected readonly MemberDbContext db;
+
+    public MemberRepository(MemberDbContext _db)
+    {
+        db = _db;
+    }
+
+    public async Task<Member> AddMember(Member member)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(member));
+        await db.Members.AddAsync(member);
+        return member;
+    }
+
+    public async Task<Member> DeleteMember(int memberId)
+    {
+        var member = await GetMemberById(memberId) ?? throw new InvalidOperationException("Member not found");
+        db.Members.Remove(member);
+        return member;
+    }
+
+    public async Task<IEnumerable<Member>> GetAllMembers()
+    {
+        var membersList = await db.Members.ToListAsync();
+        return membersList ?? Enumerable.Empty<Member>();
+    }
+
+    public async Task<Member> GetMemberById(int memberId)
+    {
+        var member = await db.Members.FindAsync(memberId) ?? throw new InvalidOperationException("Member not found");
+        return member;
+    }
+
+    public void UpdateMember(Member member)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(member));
+        db.Members.Update(member);
+    }
+}
---
+++ CleanArch-CQRS.Infra/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using CleanArch_CQRS.Domain.Abstractions;
+using CleanArch_CQRS.Infra.Context;
+
+namespace CleanArch_CQRS.Infra.Repositories;
+public class UnitOfWork : IUnitOfWork, IDisposable
+{
+    private IMemberRepository? _memberRepository;
+    private readonly MemberDbContext _memberDbContext;
+
+    public UnitOfWork(MemberDbContext memberDbContext)
+    {
+        _memberDbContext = memberDbContext;
+    }
+
+    public IMemberRepository MemberRepository
+    {
+        get
+        {
+            return _memberRepository ??= new MemberRepository(_memberDbContext);
+        }
+    }
+
+    public async Task CommitAsync()
+    {
+        await _memberDbContext.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _memberDbContext.Dispose();
+    }
+}
